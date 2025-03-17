7378d5a05e8dbb92740fb4932c9b583442d52741
Menalin <menalin4@gmail.com>
2025-03-17 16:02:41 +0530

    created tabbar

---
+++ components/HomeTabbar.tsx
@@ -0,0 +1,30 @@
+import { ProductType } from '@/constants';
+import { title } from 'process';
+import React from 'react'
+import { Repeat } from 'lucide-react';
+
+interface Props{
+    selectedTab:string;
+    onTabSelect: (tab: string) => void;
+    
+}
+
+const HomeTabbar = ({selectedTab,onTabSelect}:Props) => {
+  return (
+    <div className='flex items-center gap-1.5 text-sm font-semibold'>
+        <div className='flex items-center gap-1.5'>
+            {ProductType?.map((item) => (
+                <button key={item?.title}
+                onClick={() => onTabSelect(item?.title)}
+                className={`border border-darkColor px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-darkColor hover:text-white  ${selectedTab === item?.title && "bg-darkColor text-white"}`}
+                >{item?.title}</button>
+            ))}
+        </div>
+        <button className={`border border-darkColor p-2 rounded-full hover:bg-darkColor hover:text-white `}>
+            <Repeat className='w-5 h-5' />
+        </button>
+    </div>
+  )
+}
+
+export default HomeTabbar
--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,11 @@ export const headerData = [
 {title:"Featured",href:"/featured"},
 {title:"Gift",href:"/gift"},
 ];
+
+export const ProductType= [
+    { title: "Travel Inspired Art",value:"travelart"},
+    { title: "Boho Art",value:"bohoart"},
+    { title: "Modern Art",value:"modernart"},
+    { title: "Landscapes Art",value:"landscapesart"},
+    { title: "Nature Art",value:"natureart"},
+];
